90af2364dd85235e2b86cb30b23f785bf1854d8f
Ericka <ericka@linux-fiev.site>
2014-10-14 23:00:29 -0600

    Validacion datos

--- laravel/app/controllers/UploadController.php
+++ laravel/app/controllers/UploadController.php
@@ -20,6 +20,42 @@ class UploadController extends \BaseController {
 	}
 
 
+	public function validate(){
+
+		$data = Input::all();
+
+		$rules = array(
+			'file' => 'required',
+			'PARTS'=> 'Integer',
+			'MINUTES'=> 'Integer'
+
+			);
+
+		$validate = Validator::make($data, $rules);
+
+		if ($validate->fails()) {
+            return Redirect::to('uploads')
+                ->withErrors($validate);
+		}
+		return dd($data);
+
+
+
+
+
+		/**if($radio_btn == 'parts'){
+			$validator = Validator::make(
+    		array('radio_btn' => 'required|min:1')
+    		);
+		}
+		else if($radio_btn == 'minutes'){
+			$validator = Validator::make(
+    		array('radio_btn' => 'required|min:1')
+    		);
+		}*/
+	}
+
+
 	/**
 	 * Show the form for creating a new resource.
 	 *
